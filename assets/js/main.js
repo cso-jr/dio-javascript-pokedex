f33a278cf0b31f73990eef4e95121c1ce8163f68
Carlos Oliveira <carlos.oliveirajr@hotmail.com>
2025-12-11 22:22:10 -0300

    fetch, map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,20 @@
 
+function convertPokemonTypesToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
+}
 
 function convertPokemonToLi(pokemon) {
     return `<li class ="pokemon">
-                        <span class="number">#001</span>
+                        <span class="number">${pokemon.order}</span>
                         <span class="name">${pokemon.name}</span>
 
                         <div class="detail">   
                             <ol class="types">
-                                <li class="type">grass</li>
-                                <li class="type">poison</li>    
+                                ${convertPokemonTypesToLi(pokemon.types).join('')} 
                             </ol>
 
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
                         </div>
                     </li>
         `
@@ -25,13 +23,8 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList');
 
 
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = [];
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon);
-        listItems.push(convertPokemonToLi(pokemon));
-
-        }
-    console.log(listItems);
+pokeApi.getPokemons().then((pokemons=[]) => {
+   const newHtml = pokemons.map(convertPokemonToLi).join('');
+   pokemonList.innerHTML = newHtml;
 });
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,18 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     
     return fetch(url) /* Fetch API - faz o fetch e espera uma resposta que é uma promessa*/
     .then((response) => response.json()) /* então, a promessa recebida no fetch é convertida para JSON */
-    .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error));   
+    .then((jsonBody) => jsonBody.results)/* necessário para descartar outras informações e ficar só com o corpo da resposta*/
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))/*map da lista dos pokemons, para extrair os detalhes dos pokemons que precisam de outra chamada de url*/
+    .then((detailRequests) => Promise.all(detailRequests))/* promise.all aguarda até que tenha todas as respostas da lista para retornar*/
+    .then((pokemonsDetails) => pokemonsDetails)
+       
 }
+
